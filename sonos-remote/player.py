600a036a70404242bd76551d3160d71fea6aeb32
Timo Meinen <Timo.Meinen@tuifly.com>
2023-10-22 17:26:34 +0200

    debug

---
+++ sonos-remote/player.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+import time
+
+from soco.discovery import by_name
+from soco.plugins import sharelink
+
+from reader import Reader
+
+speaker = by_name("Move")
+reader = Reader()
+
+databaseFile = "database.txt"  # name of the local database text file
+defaultVolume = 20
+
+
+def main():
+    while True:
+        # slow down the card reading while loop
+        time.sleep(0.2)
+
+        print("Waiting for card")
+        tag = reader.reader.read_card()
+        print("Card TAG: ", tag)
+        play_nfc_stream(tag)
+
+
+def play_nfc_stream(nfcUID):
+    playlists = load_playlists()
+
+    x = 0
+    for i in playlists:
+        tag = str(i[0]).lower()
+
+        if tag == nfcUID:
+            print("Found card in database", tag)
+            service = identify_service(playlists[x])
+
+            if service == "spotify":
+                play_spotify_playlist(playlists[x])
+            elif service == "utility":
+                utility_controls(playlists[x])
+            else:
+                print("Error: Unknown Service")
+
+            break
+
+        x = x + 1
+
+
+def load_playlists():
+    global databaseFile
+
+    database = open(databaseFile, "r")
+    playlists = []
+
+    print("Load database")
+    for playlist in database:
+        print("Loaded playlist: ", playlist)
+        playlists.append(playlist.split(","))
+
+    database.close()
+
+    print(playlists)
+
+    return playlists
+
+
+def identify_service(playlists):
+    return playlists[1]
+
+
+def play_spotify_playlist(playlists):
+    playlist_name = playlists[2]
+    playlist_url = playlists[3]
+
+    media_uri = sharelink.SpotifyShare().canonical_uri(playlist_url)
+    print("Media URI", media_uri)
+
+    # speaker.clear_queue()
+    # sharelink.ShareLinkPlugin(speaker).add_share_link_to_queue(media_uri, position=1, as_next=True)
+    # speaker.play_from_queue(0, start=True)
+    speaker.play_uri(media_uri)
+
+    print("Playing the Spotify playlist: ", playlist_name)
+
+
+def utility_controls(playlists):
+    global defaultVolume
+    global speaker
+    control = playlists[2].strip()
+
+    if control == "vol reset":
+        speaker.volume = defaultVolume
+    elif control == "vol down":
+        speaker.volume = (defaultVolume - 10)
+
+
+if __name__ == "__main__":
+    main()
