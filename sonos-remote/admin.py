b8cc3ed33a4d098f331b668e8ba26687c80649f4
Timo Meinen <Timo.Meinen@tuifly.com>
2023-10-29 12:44:33 +0100

    Add Speaker Selection

--- sonos-remote/admin.py
+++ sonos-remote/admin.py
@@ -1,41 +1,68 @@
 #!/usr/bin/env python3
 
+import soco
 from soco.plugins import sharelink
+
 from reader import Reader
 
+MUSIC_SERVICE = "spotify"
 DATABASE_FILENAME = "database.txt"
+
+speakers = list(soco.discover())
 cardReader = Reader()
 
 
 def main():
-    keep_going = 1
-
-    while keep_going == 1:
+    while True:
         add_entry()
-
-        keep_going = int(input("(0) Quit or Continue?"))
-        if keep_going == 0:
-            exit(0)
+        print()
+        quit_program_prompt()
 
 
 def add_entry():
+    tag = read_nfc_tag()
+    speaker = read_speaker()
+    media_uri = read_media_uri()
+    playlist = read_playlist(media_uri, tag, speaker)
+
+    create_database_entry(playlist)
+
+
+def read_nfc_tag():
     print("Please tap NFC tag for this playlist now.")
     tag = cardReader.read_card()
-    print("Read tag: ", tag)
-    add_spotify_uri(tag)
+    return tag
 
 
-def add_spotify_uri(tag):
+def read_speaker():
+    for i, speaker in enumerate(speakers, start=1):
+        print(i, speaker.player_name)
+    speaker_index = int(input("Select speaker: "))
+    return speakers[speaker_index - 1].player_name
+
+
+def read_media_uri():
     playlist_url = input("Please paste Spotify Share URL now: ")
     media_uri = sharelink.SpotifyShare().canonical_uri(playlist_url)
+    return media_uri
 
+
+def read_playlist(media_uri, tag, speaker):
     playlist_name = input("What is this playlist called? ")
+    playlist = ','.join([tag, speaker, MUSIC_SERVICE, playlist_name, media_uri])
+    return playlist
 
-    playlist = tag + ",spotify," + playlist_name + "," + media_uri
+
+def create_database_entry(playlist):
     print("DB Entry: ", playlist)
-    database = open(DATABASE_FILENAME, "a")
-    database.write(playlist + "\n")
-    database.close()
+    with open(DATABASE_FILENAME, "a") as database:
+        database.write(playlist + "\n")
+
+
+def quit_program_prompt():
+    quit_program = input("Quit? (y/N) ")
+    if quit_program == "y":
+        exit(0)
 
 
 if __name__ == "__main__":
